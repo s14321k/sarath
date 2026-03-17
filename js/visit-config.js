d93695936e9cd34a6d19c7c1dea9221946684644
s14321k <59145492+s14321k@users.noreply.github.com>
2026-03-17 13:29:46 +0530

    changes

--- js/visit-config.js
+++ js/visit-config.js
@@ -1,4 +1,4 @@
 // Set this to your deployed worker URL, e.g. https://visit-ingest.yourname.workers.dev
 window.VISIT_ENDPOINT = "https://visit-ingest-342647168408.asia-south1.run.app";
 // Allowed origin for backend checks
-window.VISIT_ALLOWED_ORIGIN = "http://localhost:63342";
+window.VISIT_ALLOWED_ORIGIN = "https://s14321k.github.io";
