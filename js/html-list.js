d2fc7c21f04430d1ec213974117a6707ae482285
s14321k <59145492+s14321k@users.noreply.github.com>
2026-04-16 17:05:44 +0530

    update html files and enhance safe-loader with local fallback and error handling

--- js/html-list.js
+++ js/html-list.js
@@ -1,1 +1,1 @@
-window.htmlFiles = ["design-patterns.html", "microservice-architecture.html", "solid_and_design_principles_reference.html"];
+window.htmlFiles = ["design-patterns.html", "loadBalancerapiGatewaySecurity.html", "microservice-architecture.html", "solidAndDesignPrinciple.html"];
